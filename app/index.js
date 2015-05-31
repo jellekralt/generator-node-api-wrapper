0c443aa238e7256ffd8afaf6bba1fc303b7859b2
Jelle Kralt <jelle@jellekralt.nl>
2015-05-31 22:46:22 +0200

    Added prompt questions and template vars

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var slugify = require("underscore.string/slugify");
 
 module.exports = yeoman.generators.Base.extend({
   initializing: function () {
@@ -13,18 +14,50 @@ module.exports = yeoman.generators.Base.extend({
 
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the gnarly ' + chalk.red('NodeApiWrapper') + ' generator!'
+      'Welcome to the ' + chalk.red('Node API Wrapper') + ' generator!'
     ));
 
     var prompts = [{
+      type: 'input',
+      name: 'apiName',
+      message: 'The API name'
+    },{
+      type: 'input',
+      name: 'name',
+      message: 'The package name',
+      default: function(props) {
+        return 'node-' + slugify(props.apiName);
+      }
+    },{
       type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
+      name: 'addServices',
+      message: 'Would you like to split up the API methods in separate \'services\' files?',
       default: true
+    },{
+      type: 'confirm',
+      name: 'addAuth',
+      message: 'Would you like to add authentication?',
+      default: true
+    },{
+      type: 'list',
+      name: 'authType',
+      message: 'What method of authentication does this API use?',
+      choices: [
+        {
+          name: 'OAuth 2.0',
+          value: 'oauth2'
+        }, {
+          name: 'Basic Authentication',
+          value: 'basic'
+        }
+      ]
     }];
 
     this.prompt(prompts, function (props) {
       this.props = props;
+
+      this.props.packageName = slugify(props.name);
+
       // To access props later use this.props.someOption;
 
       done();
@@ -33,13 +66,15 @@ module.exports = yeoman.generators.Base.extend({
 
   writing: {
     app: function () {
-      this.fs.copy(
+      this.fs.copyTpl(
         this.templatePath('_package.json'),
-        this.destinationPath('package.json')
+        this.destinationPath('package.json'),
+        this.props
       );
       this.fs.copy(
         this.templatePath('_bower.json'),
-        this.destinationPath('bower.json')
+        this.destinationPath('bower.json'),
+        this.props
       );
     },
 
